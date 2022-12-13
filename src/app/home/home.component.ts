81dc1b73eb5517a666063e1e877a99e4ca9648a5
Priyanka Khare <75668973+PriyankaKhare8@users.noreply.github.com>
2022-12-13 18:50:15 +0530

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,7 @@ import { DataServiceService } from '../services/data-service.service';
 })
 export class HomeComponent implements OnInit {
   data: any;
+  loading: boolean = false ;
   constructor(
     private router: Router,
     private dataService: DataServiceService) { }
@@ -21,18 +22,23 @@ export class HomeComponent implements OnInit {
 
 
 
+  
   getAllEmployeeDetails() {
+    this.loading = true;
     this.dataService.getAllEmployees().subscribe(response => {
       if (response) {
         console.log(response);
         this.data = response;
         localStorage.setItem("length", this.data.length);
+        this.loading = false;
       }
     }, (error) => {
+      this.loading = false;
       alert('Get API failed')
 
     });
   }
+  
   navigate() {
     this.router.navigate(['/', 'add-employee']);
   }
@@ -40,12 +46,14 @@ export class HomeComponent implements OnInit {
   deleteEmployee(e: any) {
     if (e != null) {
       if (e.id != null || e.id != undefined) {
+        this.loading = true;
         this.dataService.removeEmployee(e.id).subscribe(response => {
           if (response) {
             alert('data removed successfully !!!!');
             this.dataService.getAllEmployees().subscribe(res => {
               if (res) {
                 this.data = res;
+                this.loading = false;  
               }
             })
           }
